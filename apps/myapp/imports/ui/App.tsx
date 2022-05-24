ddde599c11fd14f7c895371211f505a8a44ea83d
Tim van Steenis <tims@q42.nl>
2022-05-24 18:19:19 +0200

    added failing snowplow

--- apps/myapp/imports/ui/App.tsx
+++ apps/myapp/imports/ui/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MyFeature } from "@lessonup/package-a";
-// import { newTracker } from '@snowplow/browser-tracker'
+import { newTracker } from "@snowplow/browser-tracker";
 
 export const App = () => {
-  // useEffect(() => {
-  //   console.info('foo', newTracker("t1", "https://foo"));
-  // }, [])
+  useEffect(() => {
+    console.info("foo", newTracker("t1", "https://foo"));
+  }, []);
 
   return <MyFeature />;
 };
