d2cf57bce40ba0a98a801b3c86a053722c536b9d
Tim van Steenis <tims@q42.nl>
2022-05-25 10:47:17 +0200

    wip

--- packages/package-a/src/feature/MyFeature.tsx
+++ packages/package-a/src/feature/MyFeature.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { PrimaryButton } from "@lessonup/package-b";
 
-export const MyFeature = () => {
+declare var require: any;
+declare var window: any;
+
+require("react-dom");
+window.React2 = require("react");
+console.log(window.React1, window.React2, window.React1 === window.React2);
+
+export function MyFeature() {
+  const [count, setCount] = React.useState(0);
+
+  const onClick = () => {
+    setCount((value) => value + 1);
+  };
+
   return (
     <div>
-      <h1>Hi!</h1>
+      <h1>I clicked this {count} times</h1>
 
-      <PrimaryButton title="click me" onClick={() => console.info("click")} />
+      <PrimaryButton title="increment" onClick={onClick} />
     </div>
   );
-};
+}
